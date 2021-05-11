cd5b01d56b3f6ae0b8e5f55a6ecc248519967b01
meriemkhadraoui <miyakhad@gmail.com>
2021-05-11 12:27:17 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,39 @@
-setInterval(showTime, 1000);
-function showTime() {
-  let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let sec = time.getSeconds();
-  amPm = "AM";
-
-  if (hour > 12) {
-    hour -= 12;
-    amPm = "PM";
+const buttonStart = document.querySelector("#button-start");
+const buttonStop = document.querySelector("#button-stop");
+const buttonReset = document.querySelector("#button-reset");
+const second = document.querySelector("#secconds");
+const tens = document.querySelector("#tens");
+let interval;
+let conterTens = 0;
+let conterSec = 0;
+function start() {
+  conterTens++;
+  if (conterTens < 9) {
+    tens.innerHTML = "0" + conterTens;
   }
-  if (hour == 0) {
-    hr = 12;
-    amPm = "AM";
+  if (conterTens > 9) {
+    tens.innerHTML = conterTens;
   }
-
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
-
-  let currentTime = hour + ":" + min + ":" + sec + amPm;
-
-  document.querySelector(".clock").innerHTML = currentTime;
+  if (conterTens > 99) {
+    conterSec++;
+    second.innerHTML = "0" + conterSec;
+    conterTens = 0;
+    tens.innerHTML = "0" + 0;
+  }
+  if (conterSec > 9) {
+    second.innerHTML = conterSec;
+  }
 }
-showTime();
+buttonStart.onclick = function () {
+  interval = setInterval(start);
+};
+buttonStop.onclick = function () {
+  clearInterval(interval);
+};
+buttonReset.onclick = function () {
+  clearInterval(interval);
+  conterTens = "00";
+  conterSec = "00";
+  second.innerHTML = conterTens;
+  tens.innerHTML = conterSec;
+};
